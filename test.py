60106e321efd60487024012f04c29e070d440d3a
Viraj Yatish Parikh <71379449+VirajParikh1997@users.noreply.github.com>
2023-10-20 21:43:02 -0400

    sdfv

--- test.py
+++ test.py
@@ -34,7 +34,7 @@ def run_profiler_experiment(model, device, batch_size, num_tokens, embedding_dim
     with profile(activities=[ProfilerActivity.CUDA], record_shapes=True) as prof:
         with record_function("model_inference"):
             model(data)
-    cpu_time = prof.key_averages().self.gpu_time_total
+    cpu_time = prof.key_averages().self_gpu_time_total
     memory_usage = prof.key_averages().device_memory_usage
     print("GPU Time: ", cpu_time)
     print("Memory Usage: ", memory_usage)
